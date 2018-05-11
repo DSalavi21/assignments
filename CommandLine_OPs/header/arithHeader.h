95e0262f89584f9b5afc57bbecd2c925219b739f
Dhananjay Salavi <DSalavi21@users.noreply.github.com>
2018-05-11 17:09:05 +0530

    Add files via upload

---
+++ CommandLine_OPs/header/arithHeader.h
@@ -0,0 +1,18 @@
+#ifndef __ARITHHEADER_H__
+
+#define __ARITHHEADER_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> //atoi(), strtol()
+#include <errno.h>
+
+void usage();
+int Addition(const int, char** );
+int Subtraction(const int, char** );
+int Multiplication(const int, char** );
+int Division(const int, char** );
+
+
+#endif
+
