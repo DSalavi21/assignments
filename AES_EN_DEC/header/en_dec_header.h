ed6451848941c54ba5a63616347cc718f6039717
Dhananjay Salavi <DSalavi21@users.noreply.github.com>
2018-07-07 07:50:22 +0000

    Create en_dec_header.h

---
+++ AES_EN_DEC/header/en_dec_header.h
@@ -0,0 +1,10 @@
+//=============================== EN_DEC_HEADER.H   ===============================
+
+#ifndef __EN_DEC_HEADER_H__
+#define __EN_DEC_HEADER_H__
+
+int encrypt_128bit_block( char*, char*);
+int decrypt_128bit_block( char* );
+void operation_EXOR( char* );
+
+#endif
